4ea6d76461b5039e39d3527aa4c659cf9d539c8c
Phillipp Engelke <zottelchin@gmail.com>
2018-01-19 22:35:34 +0100

    updatet routine

--- hello.go
+++ hello.go
@@ -5,13 +5,10 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
-	"strconv"
 	"time"
 )
 
 var url string = "https://www.studentenwerk-magdeburg.de/mensen-cafeterien/heute-in-unseren-mensen/"
-var lastUpdateh int
-var lastUpdated int
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	content := gethtml()
@@ -22,12 +19,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func gethtml() string {
-	if lastUpdated < time.Now().Day() {
-		update()
-		fmt.Println("Data updatet")
-	} else {
-		fmt.Println("Data not updatet")
-	}
 	cont, _ := ioutil.ReadFile("menu.txt")
 	content := string(cont[:])
 	return content
@@ -35,6 +26,7 @@ func gethtml() string {
 
 func main() {
 	update()
+	go schedultUpdate()
 	fmt.Println("Got Mensa Page. Starting Server")
 	port := ":8080"
 	http.HandleFunc("/", handler)
@@ -64,14 +56,51 @@ func update() {
 	for i := 0; i < len(match); i++ {
 		match[i][0] = re3.ReplaceAllString(match[i][0], "")
 	}
-	re4 := regexp.MustCompile("<span\\sclass='grau(?:(?U).*)</strong>")
+	re4 := regexp.MustCompile("<span\\sclass='grau(?:(?U).*)<\\/strong>")
 	for i := 0; i < len(match); i++ {
 		match[i][0] = re4.ReplaceAllString(match[i][0], "</strong>")
 	}
-	lastUpdateh = time.Now().Hour()
-	lastUpdated = time.Now().Day()
-
-	content = "<p>Letztes Update der Daten am " + strconv.Itoa(lastUpdated) + ". um " + strconv.Itoa(lastUpdateh) + " Uhr</p><h4>Mensa UniCampus Magdeburg, unterer Saal</h4>"
+	re5 := regexp.MustCompile("(style=\"(?:(?U).*)\")|(<\\/tr><\\/thead><tbody>)|(<\\/tr><\\/table>)|(<\\/table>)")
+	for i := 0; i < len(match); i++ {
+		match[i][0] = re5.ReplaceAllString(match[i][0], "")
+	}
+	re6 := regexp.MustCompile("<span\\sclass='grau(?:(?U).*)<\\/span>")
+	for i := 0; i < len(match); i++ {
+		match[i][0] = re6.ReplaceAllString(match[i][0], "")
+	}
+	re7 := regexp.MustCompile("(<tr\\s?><td\\s(?:(?U).*)>)|(<table><thead><tr><td(?:(?U).*)>)")
+	for i := 0; i < len(match); i++ {
+		match[i][0] = re7.ReplaceAllString(match[i][0], "<li>")
+	}
+	re8 := regexp.MustCompile("<\\/td>")
+	for i := 0; i < len(match); i++ {
+		match[i][0] = re8.ReplaceAllString(match[i][0], "</li>")
+	}
+	re9 := regexp.MustCompile("<div(?:(?U).*)>")
+	for i := 0; i < len(match); i++ {
+		match[i][0] = re9.ReplaceAllString(match[i][0], "<div class='mensa'><ul>")
+	}
+	re10 := regexp.MustCompile("<\\/div>")
+	for i := 0; i < len(match); i++ {
+		match[i][0] = re10.ReplaceAllString(match[i][0], "</ul></div>")
+	}
+	re11 := regexp.MustCompile("\\|\\s\\d,\\d\\d")
+	for i := 0; i < len(match); i++ {
+		match[i][0] = re11.ReplaceAllString(match[i][0], "")
+	}
+	re12 := regexp.MustCompile("<br\\s\\/>")
+	for i := 0; i < len(match); i++ {
+		match[i][0] = re12.ReplaceAllString(match[i][0], " ")
+	}
+	// re13 := regexp.MustCompile("\\d,\\d\\d")
+	// for i := 0; i < len(match); i++ {
+	// match2 := re13.FindAllStringSubmatch(match[0][0], -1)
+	// for j := 0; j < len(match2); j++ {
+	// 	match[0][0] = re13.ReplaceAllString(match[0][0], match2[j][0]+"&euro;")
+	// 	re13.
+	// }
+	// }
+	content = "<h1>baremetal Mensa Plan</h1><h4>Mensa UniCampus Magdeburg, unterer Saal</h4>"
 	for i := 0; i < len(match); i++ {
 		content += match[i][0]
 	}
@@ -79,4 +108,12 @@ func update() {
 	cont = []byte(htmlStart + content + htmlEnd)
 	ioutil.WriteFile("menu.txt", cont, 0600)
 
+}
+
+func schedultUpdate() {
+	for {
+		time.Sleep(2 * time.Hour)
+		update()
+		fmt.Println("Data updated.")
+	}
 }
